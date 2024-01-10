5b0456a267a08ed8e3fa766c35d6a1bafef8b20c
Jessica Brann <jessica.brann@ibm.com>
2024-01-10 15:46:52 +0000

    add further logic tests to check for different cases

--- src/logic.test.js
+++ src/logic.test.js
@@ -1,9 +1,23 @@
 import isPrime from './logic';
 
-
-
 it("tests that prime number checker works with prime number", ()=> {
   const num = 5;
   expect(isPrime(num)).toBe(true)
 });
 
+it("tests that prime number checker works with non prime number", ()=> {
+  const num = 6;
+  expect(isPrime(num)).toBe(false)
+});
+
+it("tests that prime number checker works with 0", ()=> {
+  const num = 0;
+  expect(isPrime(num)).toBe(false)
+});
+
+it("tests that prime number checker works with non number", ()=> {
+  const num = "n";
+  expect(isPrime(num)).toBe(false)
+});
+// TODO - add in a check later on to make sure non-number entries show up as an error rather than just being false (for now, false is what we expect)
+// Same goes for the zero entry, for now we expect a false response but should really give an error when logic is developed further
