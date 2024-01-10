724510fb658720a68c4c3bd3bbcdfc8f2e2237fe
Jessica Brann <jessica.brann@ibm.com>
2024-01-10 16:17:58 +0000

    Add more complex functionality and create basic app page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  isPrime
+} from './logic';
 
-function App() {
+
+export default function App() {
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleChange = e => {
+    setValue(e.target.value);
+    setResult("");
+  }
+
+  const handleClick = _ => {
+    // if value is not a number, return result as error
+    if (isNaN(value)) {
+      setResult("ERROR");
+    } else {
+      const res = isPrime(parseInt(value, 10)) ? "YES": "NO";
+      setResult(res);
+    }
+  }
+
+  let text = "";
+
+  switch(result) {
+    case "YES":
+      text = `${value} is a prime number`;
+      break;
+    case "NO":
+      text = `${value} is not a prime number`;
+      break;
+    case "ERROR":
+      text = `${value} is not a number`;
+      break;
+    default:
+      text = `Please input a number and select 'check'`;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>Enter number to verify prime number</p>
+        <input type="text" className="input" value={value} onChange={handleChange}/>
+      </div>
+      <div>
+        <button className="button" onClick={handleClick}>Check</button>
+      </div>
+      <div>{text}</div>
     </div>
   );
 }
 
-export default App;
