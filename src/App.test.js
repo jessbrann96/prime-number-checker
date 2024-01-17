d18f120a7164a35cbf43ec5e4094bdd5b44ef6f0
Jessica Brann <jessica.brann@ibm.com>
2024-01-17 16:26:19 +0000

    Fixed failing test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import { render, screen } from '@testing-library/react';
 
 test('renders basic text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Enter number to verify prime number/i);
+  const linkElement = screen.getByText(/Enter a number to check if it is prime/i);
   expect(linkElement).toBeInTheDocument();
 });
